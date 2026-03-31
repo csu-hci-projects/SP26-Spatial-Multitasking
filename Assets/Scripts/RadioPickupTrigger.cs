20e762b389a6daa07082394a000cd3738460635e
GrahamMN <marissa.graham.n@gmail.com>
2026-03-31 14:37:25 -0600

    Requiring radio to be brought to mouth in order to bring in next ship. Added actual ship to ShipPlaceholder.

--- Assets/Scripts/RadioPickupTrigger.cs
+++ Assets/Scripts/RadioPickupTrigger.cs
@@ -4,9 +4,11 @@ using Oculus.Interaction;
 public class RadioPickupTrigger : MonoBehaviour
 {
     public ShipCaseManager shipCaseManager;
+    public Transform mouthPoint;
+    public float triggerDistance = 0.18f;
 
     private Grabbable grabbable;
-    private bool wasGrabbedLastFrame = false;
+    private bool hasActivatedThisGrab = false;
 
     void Start()
     {
@@ -15,18 +17,25 @@ public class RadioPickupTrigger : MonoBehaviour
 
     void Update()
     {
-        if (grabbable == null || shipCaseManager == null)
+        if (grabbable == null || shipCaseManager == null || mouthPoint == null)
         {
             return;
         }
 
         bool isGrabbedNow = grabbable.SelectingPointsCount > 0;
 
-        if (isGrabbedNow && !wasGrabbedLastFrame)
+        if (!isGrabbedNow)
         {
-            shipCaseManager.ActivateRadioCall();
+            hasActivatedThisGrab = false;
+            return;
         }
 
-        wasGrabbedLastFrame = isGrabbedNow;
+        float distanceToMouth = Vector3.Distance(transform.position, mouthPoint.position);
+
+        if (distanceToMouth <= triggerDistance && !hasActivatedThisGrab)
+        {
+            shipCaseManager.ActivateRadioCall();
+            hasActivatedThisGrab = true;
+        }
     }
 }
